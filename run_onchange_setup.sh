d7eb8f1aaa8e6b590f6602d4e869de2f9afbbf50
Tat1209 <77540698+Tat1209@users.noreply.github.com>
2026-03-25 23:06:49 +0900

    feat: .sh改良

--- run_onchange_setup.sh
+++ run_onchange_setup.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
 
-echo "Updating package lists and installing/updating Git & Neovim..."
-
 sudo apt-get update
-sudo apt-get install -y git neovim
+sudo apt-get install -y git neovim build-essential
